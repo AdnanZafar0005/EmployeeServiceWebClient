50041d05906a3b9408726f91c8e783fb67a46d12
Adnan Zafar <adnanzaffar0005@outlook.com>
2021-10-07 10:09:55 +0500

    initial

---
+++ EmployeeServiceWebClient/WebForm1.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EmployeeServiceWebClient.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 23px;
+        }
+    </style>
+</head>
+<body>
+
+    <form runat="server" id="Form1">
+
+  
+    <table style="font-family:Arial; border:1px solid black;">
+    <tr>
+        <td>
+            <b>ID</b>
+        </td>
+        <td>
+            <asp:TextBox ID="txtID" runat="server">
+            </asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <b>Name</b>
+        </td>
+        <td>
+            <asp:TextBox ID="txtName" runat="server">
+            </asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <b>Gender</b>
+        </td>
+        <td>
+            <asp:DropDownList ID="txtGender" runat="server" Height="20px" Width="126px">
+                <asp:ListItem>Select Gender</asp:ListItem>
+                <asp:ListItem>Male</asp:ListItem>
+                <asp:ListItem>Female</asp:ListItem>
+            </asp:DropDownList>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <b>Date Of Birth</b>
+        </td>
+        <td>
+            <asp:TextBox ID="txtDateOfBirth" runat="server">
+            </asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Button ID="btnGetEmployee" runat="server" 
+                                 Text="Get Employee" 
+                                 onclick="btnGetEmployee_Click" />                                    
+        </td>
+        <td>
+            <asp:Button ID="btnSave" runat="server" 
+                        Text="Save Employee" 
+                        onclick="btnSave_Click" />
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" class="auto-style1">
+            <asp:Label ID="lblMessage" runat="server" 
+                        ForeColor="Green" Font-Bold="true">
+            </asp:Label>
+        </td>
+    </tr>
+</table>
+ </form>
+</body>
+</html>
---
+++ EmployeeServiceWebClient/WebForm1.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmployeeServiceWebClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetEmployee_Click(object sender, EventArgs e)
+        {
+            EmployeeService.EmployeeServiceClient client = new EmployeeService.EmployeeServiceClient();
+            if(txtID.Text == "")
+            {
+                lblMessage.Text = "Please enter valid id.";
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                txtName.Text = txtID.Text = txtGender.Text = txtDateOfBirth.Text = "";
+                return;
+            }
+         
+            EmployeeService.Employee employee = client.GetEmployee(Convert.ToInt32(txtID.Text));
+
+            if(employee == null)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Employee not found.";
+                txtName.Text = txtID.Text = txtGender.Text = txtDateOfBirth.Text = "";
+                return ;
+            }
+            txtName.Text = employee.Name;
+            txtGender.Text = employee.Gender;
+            txtDateOfBirth.Text = employee.DateOfBirth.ToShortDateString();
+            lblMessage.ForeColor = System.Drawing.Color.Green;
+
+            lblMessage.Text = "Employee retrieved Successfully.";
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text != "" && txtName.Text != "" && txtDateOfBirth.Text != "" && txtGender.Text != "")
+            {
+                EmployeeService.Employee employee = new EmployeeService.Employee();
+                employee.Id = Convert.ToInt32(txtID.Text);
+                employee.Name = txtName.Text;
+                employee.Gender = txtGender.Text;
+                employee.DateOfBirth = Convert.ToDateTime(txtDateOfBirth.Text);
+
+                EmployeeService.EmployeeServiceClient client = new
+                EmployeeService.EmployeeServiceClient();
+                client.SaveEmployee(employee);
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+
+                lblMessage.Text = "Employee Saved Successfully.";
+            }
+
+            else
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Please check your all fields.";
+            }
+           
+
+           
+        }
+    }
+}
